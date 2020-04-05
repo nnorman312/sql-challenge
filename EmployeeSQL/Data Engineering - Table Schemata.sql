7d917361f2772b188504044c2cb00cc180e89ede
Nicole Norman <nnorman312@gmail.com>
2020-04-05 16:11:02 -0500

    Update

--- EmployeeSQL/Data Engineering - Table Schemata.sql
+++ EmployeeSQL/Data Engineering - Table Schemata.sql
@@ -20,32 +20,28 @@ CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    PRIMARY KEY ("emp_no")
+    "to_date" date   NOT NULL
 );
 
 CREATE TABLE "Titles" (
     "emp_no" int   NOT NULL,
     "title" varchar(30)   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    PRIMARY KEY ("emp_no")
+    "to_date" date   NOT NULL
 );
 
 CREATE TABLE "Dept_employee" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar (30)   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    PRIMARY KEY ("emp_no")
+    "to_date" date   NOT NULL
 );
 
 CREATE TABLE "Dept_manager" (
     "dept_no" varchar(30)   NOT NULL,
     "emp_no" int   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    PRIMARY KEY ("dept_no")
+    "to_date" date   NOT NULL
 );
 
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
