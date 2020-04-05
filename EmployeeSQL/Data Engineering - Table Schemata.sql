d28d4ed9b05dad65cecb6022b166f69262ac0b35
Nicole Norman <nnorman312@gmail.com>
2020-04-05 15:54:52 -0500

    Update

--- EmployeeSQL/Data Engineering - Table Schemata.sql
+++ EmployeeSQL/Data Engineering - Table Schemata.sql
@@ -6,42 +6,46 @@ CREATE TABLE "Employees" (
     "gender" varchar   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_Employees" 
-	PRIMARY KEY ("emp_no")
+    PRIMARY KEY ("emp_no")
 );
 
 CREATE TABLE "Departments" (
     "dept_no" varchar (30)   NOT NULL,
     "dept_name" varchar(30)   NOT NULL,
     CONSTRAINT "pk_Departments" 
-	PRIMARY KEY ("dept_no")
+    PRIMARY KEY ("dept_no")
 );
 
 CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+    "to_date" date   NOT NULL,
+    PRIMARY KEY ("emp_no")
 );
 
 CREATE TABLE "Titles" (
     "emp_no" int   NOT NULL,
     "title" varchar(30)   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+    "to_date" date   NOT NULL,
+    PRIMARY KEY ("emp_no")
 );
 
 CREATE TABLE "Dept_employee" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar (30)   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+    "to_date" date   NOT NULL,
+    PRIMARY KEY ("emp_no")
 );
 
 CREATE TABLE "Dept_manager" (
     "dept_no" varchar(30)   NOT NULL,
     "emp_no" int   NOT NULL,
     "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+    "to_date" date   NOT NULL,
+    PRIMARY KEY ("dept_no")
 );
 
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
